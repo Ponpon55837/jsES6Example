8f262e3ff9ade6af761be17dcf51499c59e74ca3
Upfloor5GoOnTheWay <32410486+Ponpon55837@users.noreply.github.com>
2020-09-28 09:31:03 +0800

    fix rule problem

--- inputRegex/regex.js
+++ inputRegex/regex.js
@@ -1,17 +1,17 @@
-const emailInput = document.querySelector('#email')
-const passwordInput = document.querySelector('#password')
-// console.log(emailInput.value.length)
-
 const emailRule = "^[a-zA-Z0-9]+[a-z@.]*$"
 const passwordRule = "^[a-zA-Z0-9!@#$%^&*()_+]{4,8}*$"
 
-emailInput.addEventListener('input', (mailEvent) => {
+document.querySelector('#email').addEventListener('input', (mailEvent) => {
   const { mailValid, value } = mailEvent.target
   if (value.match(emailRule)){
     console.log({value})
     document.querySelector("#values").innerHTML = ''
     document.querySelector('#submitButton').style.display = ''
-  }else {
+  }else if(value.length == 0){
+    document.querySelector("#values").innerHTML = '欄位不得為空'
+    document.querySelector('#submitButton').style.display = 'none'
+  }
+  else {
     document.querySelector("#values").innerHTML = '僅能輸入英文、數字、@ 及 . '
     document.querySelector('#submitButton').style.display = 'none'
   }
