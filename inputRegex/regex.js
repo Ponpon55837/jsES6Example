899c4ef77aa1674d3ff9b944f7a63cb0ad7a3008
Upfloor5GoOnTheWay <32410486+Ponpon55837@users.noreply.github.com>
2020-09-29 10:15:50 +0800

    update variables setting

--- inputRegex/regex.js
+++ inputRegex/regex.js
@@ -1,33 +1,35 @@
 const emailRule = "^[a-zA-Z0-9]+[a-z@.]*$"
 const passwordRule = "^[a-zA-Z0-9!@#$%^&*()_+]*$"
+const valusConent = document.querySelector("#values")
+const submitButtonContent = document.querySelector('#submitButton')
 
 document.querySelector('#email').addEventListener('input', (mailEvent) => {
   if (mailEvent.target.value.match(emailRule)){
     console.log(mailEvent.target.value)
-    document.querySelector("#values").innerHTML = ''
-    document.querySelector('#submitButton').style.display = ''
+    valusConent.innerHTML = ''
+    submitButtonContent.style.display = ''
   }else if(mailEvent.target.value.length == 0){
-    document.querySelector("#values").innerHTML = '欄位不得為空'
-    document.querySelector('#submitButton').style.display = 'none'
+    valusConent.innerHTML = '欄位不得為空'
+    submitButtonContent.style.display = 'none'
   }
   else {
-    document.querySelector("#values").innerHTML = '僅能輸入英文、數字、@ 及 . '
-    document.querySelector('#submitButton').style.display = 'none'
+    valusConent.innerHTML = '僅能輸入英文、數字、@ 及 . '
+    dsubmitButtonContent.style.display = 'none'
   }
 })
 
 document.querySelector('#password').addEventListener('input', (passwordEvent) => {
   if (passwordEvent.target.value.match(passwordRule)){
     console.log(passwordEvent.target.value)
-    document.querySelector("#values").innerHTML = ''
-    document.querySelector('#submitButton').style.display = ''
+    valusConent.innerHTML = ''
+    submitButtonContent.style.display = ''
   }else if(passwordEvent.target.value.length == 0){
-    document.querySelector("#values").innerHTML = '欄位不得為空'
-    document.querySelector('#submitButton').style.display = 'none'
+    valusConent.innerHTML = '欄位不得為空'
+    submitButtonContent.style.display = 'none'
   }
   else {
-    document.querySelector("#values").innerHTML = '僅能輸入英文、數字與部分符號 '
-    document.querySelector('#submitButton').style.display = 'none'
+    valusConent.innerHTML = '僅能輸入英文、數字與部分符號 '
+    submitButtonContent.style.display = 'none'
   }
 })
 
@@ -38,10 +40,10 @@ const checkEmptyInput = () => {
   if(inputMailValue.length >= 8 && inputPWValue.length >= 8){
     console.log('Can submit')
   }else if(inputMailValue.length < 8){
-    document.querySelector("#values").innerHTML = 'Email長度不得小於8'
-    document.querySelector('#submitButton').style.display = 'none'
+    valusConent.innerHTML = 'Email長度不得小於8'
+    submitButtonContent.style.display = 'none'
   }else if (inputPWValue.length < 8){
-    document.querySelector("#values").innerHTML = '密碼長度不得小於8'
-    document.querySelector('#submitButton').style.display = 'none'
+    valusConent.innerHTML = '密碼長度不得小於8'
+    submitButtonContent.style.display = 'none'
   }
 }
