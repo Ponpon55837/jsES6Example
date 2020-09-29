a8fd10d646b2466b6c01b49495b9279205a52f08
Upfloor5GoOnTheWay <32410486+Ponpon55837@users.noreply.github.com>
2020-09-29 09:12:28 +0800

    add password regex

--- inputRegex/regex.js
+++ inputRegex/regex.js
@@ -1,13 +1,12 @@
 const emailRule = "^[a-zA-Z0-9]+[a-z@.]*$"
-const passwordRule = "^[a-zA-Z0-9!@#$%^&*()_+]{4,8}*$"
+const passwordRule = "^[a-zA-Z0-9!@#$%^&*()_+]*$"
 
 document.querySelector('#email').addEventListener('input', (mailEvent) => {
-  const { mailValid, value } = mailEvent.target
-  if (value.match(emailRule)){
-    console.log({value})
+  if (mailEvent.target.value.match(emailRule)){
+    console.log(mailEvent.target.value)
     document.querySelector("#values").innerHTML = ''
     document.querySelector('#submitButton').style.display = ''
-  }else if(value.length == 0){
+  }else if(mailEvent.target.value.length == 0){
     document.querySelector("#values").innerHTML = '欄位不得為空'
     document.querySelector('#submitButton').style.display = 'none'
   }
@@ -17,12 +16,32 @@ document.querySelector('#email').addEventListener('input', (mailEvent) => {
   }
 })
 
+document.querySelector('#password').addEventListener('input', (passwordEvent) => {
+  if (passwordEvent.target.value.match(passwordRule)){
+    console.log(passwordEvent.target.value)
+    document.querySelector("#values").innerHTML = ''
+    document.querySelector('#submitButton').style.display = ''
+  }else if(passwordEvent.target.value.length == 0){
+    document.querySelector("#values").innerHTML = '欄位不得為空'
+    document.querySelector('#submitButton').style.display = 'none'
+  }
+  else {
+    document.querySelector("#values").innerHTML = '僅能輸入英文、數字與部分符號 '
+    document.querySelector('#submitButton').style.display = 'none'
+  }
+})
+
+
 const checkEmptyInput = () => {
-  let inputValue = document.querySelector('#email').value
-  if(inputValue.length >= 8){
+  let inputMailValue = document.querySelector('#email').value
+  let inputPWValue = document.querySelector('#password').value
+  if(inputMailValue.length >= 8 && inputPWValue.length >= 8){
     console.log('Can submit')
-  }else if(inputValue.length < 8){
-    document.querySelector("#values").innerHTML = '欄位長度不得小於8'
+  }else if(inputMailValue.length < 8){
+    document.querySelector("#values").innerHTML = 'Email長度不得小於8'
+    document.querySelector('#submitButton').style.display = 'none'
+  }else if (inputPWValue.length < 8){
+    document.querySelector("#values").innerHTML = '密碼長度不得小於8'
     document.querySelector('#submitButton').style.display = 'none'
   }
 }
