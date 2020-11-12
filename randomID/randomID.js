57b7907b606235c3017036fa71119b8172f899e1
Upfloor5GoOnTheWay <32410486+Ponpon55837@users.noreply.github.com>
2020-11-12 15:35:09 +0800

    Update randomID.js
    add comment

--- randomID/randomID.js
+++ randomID/randomID.js
@@ -1,4 +1,5 @@
 const randomKeyValue = () => {
+  // 先隨機產生一個數值，轉成長度為36的字串，擷取其中第2到第10的值，並加上當天的日期轉成長度為36的字串並取第2到第10的值
   return Math.random().toString(36).substr(2,10) + Date.now().toString(36).substr(4,10)
 }
 
