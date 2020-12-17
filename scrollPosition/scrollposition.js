420c86662f1ce2a6e68283434ff18703e3397067
Upfloor5 <32410486+Ponpon55837@users.noreply.github.com>
2020-12-17 13:10:07 +0800

    update css for position and move smooth

--- scrollPosition/scrollposition.js
+++ scrollPosition/scrollposition.js
@@ -3,10 +3,10 @@ const navbarPosition = document.querySelector('#navbar')
 document.addEventListener('scroll', () => {
   let scrolled = document.scrollingElement.scrollTop
   let position = navbarPosition.offsetTop
-  if(scrolled > position + 60){
+  if(scrolled > position){
     navbarPosition.classList.add('opacityReduce')
   }
-  if (scrolled <= position + 60) {
+  if (scrolled <= position) {
     navbarPosition.classList.remove('opacityReduce')
   }
 })
