ed123c08ac25c967810af4e5b00d968cbe299339
Upfloor5 <32410486+Ponpon55837@users.noreply.github.com>
2021-01-29 09:23:48 +0800

    add form submit function

--- formValid/valid.js
+++ formValid/valid.js
@@ -56,3 +56,28 @@ getPasswordConfirm.addEventListener('input', confirm => {
     show.innerHTML = ''
   }
 })
+
+window.addEventListener( "load", () => {
+  const sendData = () => {
+    const XHR = new XMLHttpRequest()
+    // Bind the FormData object and the form element
+    const FD = new FormData(form)
+    // Define what happens on successful data submission
+    XHR.addEventListener( "load", lo => {
+      alert( event.target.responseText )
+    } )
+    // Define what happens in case of error
+    XHR.addEventListener( "error", err => {
+      alert( 'Oops! Something went wrong.' )
+    } )
+    // Set up our request
+    XHR.open( "POST", "https://example.com/cors.php" )
+    // The data sent is what the user provided in the form
+    XHR.send( FD )
+  }
+  // ...and take over its submit event.
+  getSubBtn.addEventListener('click', sub => {
+    sub.preventDefault()
+    sendData()
+  })
+})
